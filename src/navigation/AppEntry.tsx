7880763375c0d709eb720bb83a7a256b2c3b34eb
Neeraj Jangra <neeraj.jangra@grapecity.com>
2025-05-29 17:49:12 +0530

    feature/HomeScreen: Adding Tab navigation

--- src/navigation/AppEntry.tsx
+++ src/navigation/AppEntry.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import TabNavigator from './TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +14,8 @@ const AppEntry = () => {
     <NavigationContainer>
         <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
             <Stack.Screen name="Login" component={LoginScreen}/>
-            <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Home" component={TabNavigator} />
         </Stack.Navigator>
     </NavigationContainer>
   )
